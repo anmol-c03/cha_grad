e09a54df4420b0c068bd83e0ec4724167e6d6378
Anmol chalise <anmolc650@gmail.com>
2024-10-09 16:23:14 +0545

    replacing torch with chagrad

--- src/mnist.py
+++ src/mnist.py
@@ -1,18 +1,20 @@
 from scratch import x_test,model,batch_size
-import torch
-import torch.nn.functional as F
-
+import numpy as np
+from cha_grad.tensor import Tensor
 def eval(x_test,batch_size):
 
-    samp=torch.randint(0,len(x_test),(batch_size,))
+    samp=np.random.randint(0,len(x_test),(batch_size,))
     x,y=x_test[samp]
-    logits=model(x)
-    pred=torch.argmax(logits,dim=-1)
-    loss=F.cross_entropy(logits,y)
-    print(y)
-    print(pred)
-    acc=(pred==y).float().mean()
-    print(acc)
-    print(loss)
+    x,y=np.array(x),np.array(y)
+    Y=np.zeros((len(samp),10),np.float32)
+    Y[range(len(samp)),y]=-1
+    logits=model(Tensor(x))
+    pred=np.argmax(logits.data,1)
+    loss=logits.cross_entropy(Tensor(Y))
+    print('y is',y)
+    print('pred is ',pred)
+    acc=(pred==y).astype(float).mean()
+    print(f'acc is {acc:.2f}')
+    print(f'Total_loss {loss.data.item():.2f}')
 
 eval(x_test,batch_size)
